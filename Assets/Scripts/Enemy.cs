0d6391238fc38464dd6c657a96784596d3a117b3
Klaus Kazlauskas <klausmachado@gmail.com>
2016-06-02 23:44:53 -0300

    Enemy animator controller

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,6 +12,8 @@ public class Enemy : MovingObject {
 
 	// Use this for initialization
 	protected override void Start () {
+        GameManager.instance.AddEnemyToList(this);
+
         animator = GetComponent<Animator>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
 
@@ -47,6 +49,8 @@ public class Enemy : MovingObject {
     protected override void OnCantMove<T>(T component)
     {
         Player hitPlayer = component as Player;
+
+        animator.SetTrigger("enemyAttack");
 
         hitPlayer.LoseFood(playerDamage);
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
+    public float turnDelay = .1f;
     public static GameManager instance = null;
     public BoardManager boardScript;
     public int playerFoodPoints = 100;
@@ -11,6 +13,8 @@ public class GameManager : MonoBehaviour {
     public bool playersTurn = true;
 
     private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
 
 	// Use this for initialization
 	void Awake () {
@@ -20,18 +24,51 @@ public class GameManager : MonoBehaviour {
             Destroy(gameObject);
 
         DontDestroyOnLoad(gameObject);
+        enemies = new List<Enemy>();
         boardScript = GetComponent<BoardManager>();
-
         InitGame();
 	}
 
     void InitGame()
     {
+        enemies.Clear();
         boardScript.SetupScene(level);
     }
 
     public void GameOver()
     {
         enabled = false;
+    }
+
+    void Update()
+    {
+        if (playersTurn || enemiesMoving)
+            return;
+
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
     }
 }
