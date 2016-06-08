77ff260378a5a244246e200854059f283298e226
Klaus Kazlauskas <klausmachado@gmail.com>
2016-06-08 00:33:49 -0300

    Touch Controlls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,6 +23,8 @@ public class Player : MovingObject {
     private Animator animator;
     private int food;
 
+    private Vector2 touchOrigin = -Vector2.one;
+
 	// Use this for initialization
 	protected override void Start ()
     {
@@ -47,11 +49,40 @@ public class Player : MovingObject {
         int horizontal = 0;
         int vertical = 0;
 
+    #if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEB_PLAYER
+
         horizontal = (int) Input.GetAxisRaw("Horizontal");
         vertical = (int) Input.GetAxisRaw("Vertical");
 
         if (horizontal != 0)
             vertical = 0;
+
+    #else
+
+        if (Input.touchCount > 0)
+        {
+            Touch myTouch = Input.touches[0];
+
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                touchOrigin = myTouch.position;
+            }
+
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                Vector2 touchEnd = myTouch.position;
+                float x = touchEnd.x - touchOrigin.x;
+                float y = touchEnd.y - touchOrigin.y;
+                touchOrigin.x = -1;
+
+                if (Math.Abs(x) > Math.Abs(y))
+                    horizontal = x > 0 ? 1 : -1;
+                else
+                    vertical = y > 0 ? 1 : -1;
+            }
+        }
+
+    #endif
 
         if (horizontal != 0 || vertical != 0)
             AttemptMove<Wall>(horizontal, vertical);
