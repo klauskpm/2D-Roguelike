03db0e5dd807a3549624690ed86dd4923f061133
Klaus Kazlauskas <klausmachado@gmail.com>
2016-06-07 23:11:48 -0300

    Player sounds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,14 @@ public class Player : MovingObject {
     public float restartLevelDelay = 1f;
     public Text foodText;
 
+    public AudioClip moveSound1;
+    public AudioClip moveSound2;
+    public AudioClip eatSound1;
+    public AudioClip eatSound2;
+    public AudioClip drinkSound1;
+    public AudioClip drinkSound2;
+    public AudioClip gameOverSound;
+
     private Animator animator;
     private int food;
 
@@ -58,6 +66,11 @@ public class Player : MovingObject {
 
         RaycastHit2D hit;
 
+        if (Move(xDir, yDir, out hit))
+        {
+            SoundManager.instance.RandomizeSfx(moveSound1, moveSound2);
+        }
+
         CheckIfGameOver();
 
         GameManager.instance.playersTurn = false;
@@ -74,12 +87,14 @@ public class Player : MovingObject {
         {
             food += pointsPerFood;
             foodText.text = "+  " + pointsPerFood + " Food: " + food;
+            SoundManager.instance.RandomizeSfx(eatSound1, eatSound2);
             other.gameObject.SetActive(false);
         }
         else if (other.tag == "Soda")
         {
             food += pointsPerSoda;
             foodText.text = "+  " + pointsPerSoda + " Food: " + food;
+            SoundManager.instance.RandomizeSfx(drinkSound1, drinkSound2);
             other.gameObject.SetActive(false);
         }
     }
@@ -107,6 +122,10 @@ public class Player : MovingObject {
     private void CheckIfGameOver()
     {
         if (food <= 0)
+        {
+            SoundManager.instance.PlaySingle(gameOverSound);
+            SoundManager.instance.musicSource.Stop();
             GameManager.instance.GameOver();
+        }
     }
 }
